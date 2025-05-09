75b377fa1f0bfa3bfe67b7f977d86aaf0dbc555e
heshaofu2 <heshaofu2@sina.com>
2025-05-09 21:40:42 +0800

    update the display styles fo protocol

--- src/web/src/Pages/NewChatPage/Components/Pay/PayModalFooter.tsx
+++ src/web/src/Pages/NewChatPage/Components/Pay/PayModalFooter.tsx
@@ -5,29 +5,47 @@ import { useTranslation } from 'react-i18next';
 
 export const PayModalFooter = ({ className }) => {
   const { t } = useTranslation();
+  
+  const renderVirtualProductPoint1 = () => {
+    const point1Text = t('pay.virtualProductPoint1');
+    const parts = point1Text.split(/\{\{modelServiceAgreement\}\}|\{\{userPrivacyPolicy\}\}/g);
+    
+    if (parts.length !== 3) {
+      return point1Text;
+    }
+    
+    return (
+      <>
+        {parts[0]}
+        <a
+          className={styles.protocolLink}
+          href="/useragreement"
+          target="_blank"
+          referrerPolicy="no-referrer"
+          rel="noopener"
+        >
+          {t('pay.modelServiceAgreement')}
+        </a>
+        {parts[1]}
+        <a
+          className={styles.protocolLink}
+          href="/privacypolicy"
+          target="_blank"
+          referrerPolicy="no-referrer"
+          rel="noopener"
+        >
+          {t('pay.userPrivacyPolicy')}
+        </a>
+        {parts[2]}
+      </>
+    );
+  };
+  
   return (
     <div className={classNames(styles.protocolWrapper, className)}>
       <div className={styles.virtualProductDesc}>
         <div className={styles.descTitle}>{t('pay.virtualProductDesc')}</div>
-        <div className={styles.descPoint}>{t('pay.virtualProductPoint1')}</div>
-        <div className={styles.descPoint}>
-          <a
-            className={styles.protocolLink}
-            href="/useragreement"
-            target="_blank"
-            referrerPolicy="no-referrer"
-          >
-            {t('pay.modelServiceAgreement')}
-          </a>
-          <a
-            className={styles.protocolLink}
-            href="/privacypolicy"
-            target="_blank"
-            referrerPolicy="no-referrer"
-          >
-            {t('pay.userPrivacyPolicy')}
-          </a>
-        </div>
+        <div className={styles.descPoint}>{renderVirtualProductPoint1()}</div>
         <div className={styles.descPoint}>{t('pay.virtualProductPoint2')}</div>
       </div>
     </div>
