17c8f8b5c7cd90e1d614ac1de9bb30d6b0c09658
heshaofu2 <heshaofu2@sina.com>
2025-05-09 16:33:16 +0800

    feat: Add the purchase instructions and agreement link for virtual goods to the payment pop-up window

--- src/web/src/Pages/NewChatPage/Components/Pay/PayModalFooter.tsx
+++ src/web/src/Pages/NewChatPage/Components/Pay/PayModalFooter.tsx
@@ -2,28 +2,33 @@ import { memo } from 'react';
 import styles from './PayModalFooter.module.scss';
 import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
+
 export const PayModalFooter = ({ className }) => {
   const { t } = useTranslation();
   return (
     <div className={classNames(styles.protocolWrapper, className)}>
-      <div className={styles.protocolDesc}>{t('pay.protocolDesc')}</div>
-      <div className={styles.protocolLinks}>
-        <a
-          className={styles.protocolLink}
-          href="/useragreement"
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          {t('pay.modelServiceAgreement')}
-        </a>
-        <a
-          className={styles.protocolLink}
-          href="/privacypolicy"
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          {t('pay.userPrivacyPolicy')}
-        </a>
+      <div className={styles.virtualProductDesc}>
+        <div className={styles.descTitle}>{t('pay.virtualProductDesc')}</div>
+        <div className={styles.descPoint}>{t('pay.virtualProductPoint1')}</div>
+        <div className={styles.descPoint}>
+          <a
+            className={styles.protocolLink}
+            href="/useragreement"
+            target="_blank"
+            referrerPolicy="no-referrer"
+          >
+            {t('pay.modelServiceAgreement')}
+          </a>
+          <a
+            className={styles.protocolLink}
+            href="/privacypolicy"
+            target="_blank"
+            referrerPolicy="no-referrer"
+          >
+            {t('pay.userPrivacyPolicy')}
+          </a>
+        </div>
+        <div className={styles.descPoint}>{t('pay.virtualProductPoint2')}</div>
       </div>
     </div>
   );
